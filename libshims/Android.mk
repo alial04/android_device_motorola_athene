faa9f9ea196567f89a41e51fd9eda5fde9c043e4
Nikit Sharma <nikitsharma92@gmail.com>
2017-09-07 14:57:52 +0530

    athene: Rip IMS

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -32,14 +32,6 @@ LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
 
-# IMS
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := MediaBuffer.c
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation
-LOCAL_MODULE := libshim_ims
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
-
 # RIL
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := sensor.cpp
